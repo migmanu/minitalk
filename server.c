c922c9b336d3330977bdf6ec528cb3dcb6114d42
migmanu42 <jmanuelmigoya@gmail.com>
2023-09-20 18:50:55 +0200

    Testing

--- server.c
+++ server.c
@@ -6,7 +6,19 @@
 /*   By: jmigoya- <jmigoya-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 17:51:03 by jmigoya-          #+#    #+#             */
-/*   Updated: 2023/09/20 17:51:04 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2023/09/20 18:04:30 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(void)
+{
+    pid_t   pid;
+
+    pid = getpid();
+    printf("%u\n", pid);
+    return (0);
+}
